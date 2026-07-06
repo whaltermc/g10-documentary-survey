a821ea14ff3f8537fcf9373be599a5a5741323aa
WhalterMC <154499206+whaltermc@users.noreply.github.com>
2026-07-06 13:04:01 +0000

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,22 @@
 import { defineConfig } from 'vite'
-import { resolve } from 'path'
+import { dirname, resolve } from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        admin: resolve(__dirname, 'admin.html'),
-      },
-    },
-  },
-  server: {
-    port: 3000,
-  }
-})
+      outDir: 'dist',
+          emptyOutDir: true,
+              rollupOptions: {
+                    input: {
+                            main: resolve(__dirname, 'index.html'),
+                                    admin: resolve(__dirname, 'admin.html'),
+                                          },
+                                              },
+                                                },
+                                                  server: {
+                                                      port: 3000,
+                                                        }
+                                                        })
+                                                        
